ed7049e30b53d9c0a47c82dbfd06b8e2695213d7
hbaydevops <145389402+hbaydevops@users.noreply.github.com>
2024-03-07 17:31:42 -0500

    Create jenkins-pipeline.sh

---
+++ jenkins-pipeline.sh
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+    
+    parameters {
+    booleanParam description: 'specify if the job is done', name: 'true'
+    choice choices: ['Cameroon', 'Nigeria', 'America', 'Ghana', 'France'], 
+    description: 'My country of origin',
+    name: 'country'
+    string defaultValue: 'pinneaple and orange', 
+    description: 'my preferred meal',
+    name: 'fruit'
+    }
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+                echo "It's $true that the struggle is over. I am originally from $country and my preferred meal is $fruit"
+            }
+        }
+    }
+}
